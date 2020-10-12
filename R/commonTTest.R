9596b6533dc5dd1d43e0d8958f0c1d6f98512c41
vandenman <donvdbergh@hotmail.com>
2020-10-12 10:53:19 +0200

    move summarySE to ttest

--- R/commonTTest.R
+++ R/commonTTest.R
@@ -303,3 +303,143 @@
   
   return(Result)
 }
+
+summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE, conf.interval=.95, .drop=TRUE,
+                      errorBarType="confidenceInterval", usePooledSE=FALSE,
+                      dependentName = "", subjectName = "") {
+
+  # New version of length which can handle NA's: if na.rm==T, don't count them
+  length2 <- function (x, na.rm=FALSE) {
+    if (na.rm) {
+      sum(!is.na(x))
+    } else {
+      length(x)
+    }
+  }
+
+  # This does the summary. For each group's data frame, return a vector with
+  # N, mean, and sd
+  # First aggregate over unused RM factors, if desired:
+  if (usePooledSE && measurevar == dependentName) {
+
+    data <- plyr::ddply(data, c(subjectName, groupvars), plyr::summarise, dependent = mean(JaspColumn_.dependent._Encoded))
+    names(data)[which(names(data) == "dependent")] <- measurevar
+
+  } else if (usePooledSE && measurevar == paste0(dependentName, "_norm")) {
+
+    data <- plyr::ddply(data, c(subjectName, groupvars), plyr::summarise, dependent = mean(JaspColumn_.dependent._Encoded_norm))
+    names(data)[which(names(data) == "dependent")] <- measurevar
+  }
+
+  datac <- plyr::ddply(data, groupvars, .drop=.drop,
+                       .fun = function(xx, col) {
+                         c(N    = length2(xx[[col]], na.rm=na.rm),
+                           mean = mean   (xx[[col]], na.rm=na.rm),
+                           sd   = sd     (xx[[col]], na.rm=na.rm)
+                         )
+                       },
+                       measurevar
+  )
+
+  # Rename the "mean" column
+  datac <- plyr::rename(datac, c("mean" = measurevar))
+
+  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+
+  # Confidence interval multiplier for standard error
+  # Calculate t-statistic for confidence interval:
+  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+  datac$ci <- datac$se * ciMult
+
+  if (errorBarType == "confidenceInterval") {
+
+    datac$ciLower <- datac[,measurevar] - datac[,"ci"]
+    datac$ciUpper <- datac[,measurevar] + datac[,"ci"]
+
+  } else {
+
+    datac$ciLower <- datac[,measurevar] - datac[,"se"]
+    datac$ciUpper <- datac[,measurevar] + datac[,"se"]
+
+  }
+
+  return(datac)
+}
+
+.normDataWithin <- function(data=NULL, idvar, measurevar, betweenvars=NULL, na.rm=FALSE, .drop=TRUE) {
+
+  # Measure var on left, idvar + between vars on right of formula.
+  data.subjMean <- plyr::ddply(data, c(idvar, betweenvars), .drop=.drop,
+                               .fun = function(xx, col, na.rm) {
+                                 c(subjMean = mean(xx[,col], na.rm=na.rm))
+                               },
+                               measurevar,
+                               na.rm
+  )
+
+  # Put the subject means with original data
+  data <- base::merge(data, data.subjMean)
+
+  # Get the normalized data in a new column
+  measureNormedVar <- paste(measurevar, "_norm", sep="")
+  data[,measureNormedVar] <- data[,measurevar] - data[,"subjMean"] +
+    mean(data[,measurevar], na.rm=na.rm)
+
+  # Remove this subject mean column
+  data$subjMean <- NULL
+
+  return(data)
+}
+
+summarySEwithin <- function(data=NULL, measurevar, betweenvars=NULL, withinvars=NULL, idvar=NULL, na.rm=FALSE,
+                            conf.interval=.95, .drop=TRUE, errorBarType="confidenceInterval", usePooledSE=FALSE,
+                            dependentName = .BANOVAdependentName,
+                            subjectName = .BANOVAsubjectName) {
+
+  # Get the means from the un-normed data
+  datac <- summarySE(data, measurevar, groupvars=c(betweenvars, withinvars), na.rm=na.rm,
+                     conf.interval=conf.interval, .drop=.drop, errorBarType=errorBarType, usePooledSE=usePooledSE,
+                     dependentName = dependentName, subjectName = subjectName)
+  # Drop all the unused columns (these will be calculated with normed data)
+  datac$sd <- NULL
+  datac$se <- NULL
+  datac$ci <- NULL
+  datac$ciLower <- NULL
+  datac$ciUpper <- NULL
+
+  # Norm each subject's data
+  ndata <- .normDataWithin(data, idvar, measurevar, betweenvars, na.rm, .drop=.drop)
+
+  # This is the name of the new column
+  measurevar_n <- paste(measurevar, "_norm", sep="")
+
+  # Collapse the normed data - now we can treat between and within vars the same
+  ndatac <- summarySE(ndata, measurevar_n, groupvars=c(betweenvars, withinvars), na.rm=na.rm, conf.interval=conf.interval, .drop=.drop, errorBarType=errorBarType,
+                      usePooledSE=usePooledSE, dependentName = dependentName, subjectName = subjectName)
+
+  # Apply correction from Morey (2008) to the standard error and confidence interval
+  # Get the product of the number of conditions of within-S variables
+  nWithinGroups    <- prod(vapply(ndatac[,withinvars, drop=FALSE], FUN=nlevels, FUN.VALUE=numeric(1)))
+  correctionFactor <- sqrt( nWithinGroups / (nWithinGroups-1) )
+
+  # Apply the correction factor
+  ndatac$sd <- ndatac$sd * correctionFactor
+  ndatac$se <- ndatac$se * correctionFactor
+  ndatac$ci <- ndatac$ci * correctionFactor
+
+  if (errorBarType == "confidenceInterval") {
+
+    ndatac$ciLower <- datac[,measurevar] - ndatac[,"ci"]
+    ndatac$ciUpper <- datac[,measurevar] + ndatac[,"ci"]
+
+  } else {
+
+    ndatac$ciLower <- datac[,measurevar] - ndatac[,"se"]
+    ndatac$ciUpper <- datac[,measurevar] + ndatac[,"se"]
+
+  }
+
+  # Combine the un-normed means with the normed results
+  merge(datac, ndatac)
+}
