48b23567f38cc69f93e4aa7342271588310fca4a
AlexanderLyNL <a.ly@jasp-stats.org>
2021-03-24 10:02:18 +0100

    jonas test verification implemented (#40)
    * jonas test implemented
    
    - One-sample t-test not yet, wait for revert
    
    * check on data not the whole formatting of the table as well
    
    * Don's bidding
    
    * Update test-ttestpairedsamplesVerification.R
    
    * Added verification unit test one-sample t-test
    
    * changed test verified name

--- R/alexanderZTest.R
+++ R/alexanderZTest.R
@@ -1,3 +1,22 @@
+# Copyright (C) 2013-2021 University of Amsterdam
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#' The plan of this file is to successively abstract functionality
+#' from the individual t-tests into a common interface to reduce clutter
+#'
 .z.test <- function(x, y=NULL, alternative="two.sided", mu=0, sigma.x=1, sigma.y=1, 
                     ciValueMeanDiff=0.95, paired=FALSE, ciValueESMeanDiff=ciValueMeanDiff) {
   if (!is.null(y)) 
---
+++ tests/testthat/test-verified-ttestonesample.R
@@ -0,0 +1,25 @@
+context("One Sample TTest -- Verification project")
+
+
+# https://jasp-stats.github.io/jasp-verification-project/t-tests.html#one-sample-t-test
+test_that("Main table results match R, SPSS, SAS and MiniTab", { 
+  options <- jaspTools::analysisOptions("TTestOneSample")
+  
+  options$testValue <- 3
+  options$meanDifference <- TRUE
+  options$effectSize <- TRUE
+  options$meanDiffConfidenceIntervalCheckbox <- TRUE
+  options$effSizeConfidenceIntervalCheckbox <- TRUE
+  options$variables <- "V1"
+  
+  results <- jaspTools::runAnalysis("TTestOneSample", "OSTT.csv", options)
+  
+  resultTable <- results$results$ttest$data
+  makeTestTable(resultTable)
+  
+  jaspTools::expect_equal_tables(
+    "test"=resultTable,
+    "ref"=list("FALSE", 2, 99, 1.65788291072626, 1.80157830484136, 2, 1.50644485946017e-36,
+               20, 2.33846083735979, 2.19842169515864, "V1")
+  )
+})
