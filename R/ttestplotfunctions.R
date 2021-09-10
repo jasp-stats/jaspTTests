7eef2d38ad28db879067846cc20e76ef90ce3eeb
vandenman <donvdbergh@hotmail.com>
2021-09-10 14:31:37 +0200

    put .base_breaks_y2 back

---
+++ R/ttestplotfunctions.R
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2013-2018 University of Amsterdam
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+.base_breaks_y2 <- function(x, testValue){
+
+  values <- c(testValue, x$ciLower, x$ciUpper)
+  ci.pos <- c(min(values), max(values))
+  b <- pretty(ci.pos)
+  d <- data.frame(x=-Inf, xend=-Inf, y=min(b), yend=max(b))
+  list(ggplot2::geom_segment(data=d, ggplot2::aes(x=x, y=y, xend=xend, yend=yend), inherit.aes=FALSE, size = 1),
+       ggplot2::scale_y_continuous(breaks=c(min(b), testValue, max(b))))
+}
