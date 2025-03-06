eb79c0b288c2d393a6677d6f1e11fcbe91d4b122
Don van den Bergh <donvdbergh@hotmail.com>
2025-03-06 11:18:04 +0100

    Revert "add variance errorcheck indep t-test (#301)"
    This reverts commit 67a04c70dd552304d8022d00739da2ba657d549f.

--- R/commonTTest.R
+++ R/commonTTest.R
@@ -65,13 +65,11 @@ gettextf <- function(fmt, ..., domain = NULL)  {
   else if(type == "independent") {
     if (length(options$dependent) != 0 && options$group != '')
       .hasErrors(dataset,
-                 type = c('variance', 'factorLevels'),
-                 all.target = options$dependent,
+                 type = 'factorLevels',
                  factorLevels.target  = options$group,
                  factorLevels.amount  = '!= 2',
-                 variance.grouping = options$group,
                  exitAnalysisIfErrors = TRUE)
-  }
+      }
 }
 
 .ttestOptionsList <- function(options, type){
